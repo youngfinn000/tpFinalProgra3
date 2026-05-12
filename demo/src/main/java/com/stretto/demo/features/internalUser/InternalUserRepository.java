c3ba96174217e0a6db777ad3230fe8e19f4b713e
Emanuel Bustos <fakuj305@gmail.com>
2026-05-12 11:51:37 -0300

    feat:Iniciacion del Repository (terminar)

--- demo/src/main/java/com/stretto/demo/features/internalUser/InternalUserRepository.java
+++ demo/src/main/java/com/stretto/demo/features/internalUser/InternalUserRepository.java
@@ -1,4 +1,5 @@
 package com.stretto.demo.features.internalUser;
 
+//Es una interfaz que extiende JpaRepository. Spring genera la implementación automáticamente, vos solo declarás los métodos que necesitás.
 public interface InternalUserRepository {
 }

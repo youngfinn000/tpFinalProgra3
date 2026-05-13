5d84a688b9e45b03d2e07aa512c0e1f412af9c89
Emanuel Bustos <fakuj305@gmail.com>
2026-05-13 20:45:03 -0300

    feat: Creacion del Service y se agrego un metodo para buscar por fecha en el Repository

--- demo/src/main/java/com/stretto/demo/features/stockModification/StockModificationRepository.java
+++ demo/src/main/java/com/stretto/demo/features/stockModification/StockModificationRepository.java
@@ -3,5 +3,9 @@ package com.stretto.demo.features.stockModification;
 import com.stretto.demo.features.stockModification.domain.StockModificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockModificationRepository extends JpaRepository<StockModificationEntity, Long> {
+    List<StockModificationEntity> findByModificationDate(LocalDate date);
 }
--- demo/src/main/java/com/stretto/demo/features/stockModification/StockModificationService.java
+++ demo/src/main/java/com/stretto/demo/features/stockModification/StockModificationService.java
@@ -1,4 +1,22 @@
 package com.stretto.demo.features.stockModification;
 
+import com.stretto.demo.features.stockModification.domain.dto.StockModificationDTORequest;
+import com.stretto.demo.features.stockModification.domain.dto.StockModificationDTOResponse;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockModificationService {
+
+    StockModificationDTOResponse create(StockModificationDTORequest request);
+
+    List<StockModificationDTOResponse> findAll();
+
+    StockModificationDTOResponse findById(Long id);
+
+    StockModificationDTOResponse update(Long id, StockModificationDTORequest request);
+
+    void delete(Long id);
+
+    List<StockModificationDTOResponse> findByDate(LocalDate date);
 }

d114e642146c46c3556367c4f0578fc19320454b
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 10:51:27 -0300

    feat: Creacion del service

--- demo/src/main/java/com/stretto/demo/features/recipe/RecipeService.java
+++ demo/src/main/java/com/stretto/demo/features/recipe/RecipeService.java
@@ -1,4 +1,18 @@
 package com.stretto.demo.features.recipe;
 
+import com.stretto.demo.features.product.domain.dto.ProductDTOResponse;
+import com.stretto.demo.features.recipe.domain.dto.RecipeDTORequest;
+import com.stretto.demo.features.recipe.domain.dto.RecipeDTOResponse;
+
+import java.util.List;
+
 public interface RecipeService {
+
+    RecipeDTOResponse create(RecipeDTORequest request);
+
+    List<RecipeDTOResponse> findAll();
+
+    RecipeDTOResponse findById(Long id);
+
+    RecipeDTOResponse update(RecipeDTORequest request, Long id);
 }

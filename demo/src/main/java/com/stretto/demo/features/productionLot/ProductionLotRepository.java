10921675c0e7037ac82ad86e56b649c72fc3f629
Emanuel Bustos <fakuj305@gmail.com>
2026-05-13 22:12:12 -0300

    fix: Se arreglo el Repository

--- demo/src/main/java/com/stretto/demo/features/productionLot/ProductionLotRepository.java
+++ demo/src/main/java/com/stretto/demo/features/productionLot/ProductionLotRepository.java
@@ -1,8 +1,8 @@
 package com.stretto.demo.features.productionLot;
 
+import com.stretto.demo.features.productionLot.domain.ProductionLotEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductionLotRepository extends JpaRepository<ProductionLotRepository, Long> {
+public interface ProductionLotRepository extends JpaRepository<ProductionLotEntity, Long> {
 }

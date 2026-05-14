e0d70f6d6d7ceb7c31bddb9979a090ebdd473eb9
Emanuel Bustos <fakuj305@gmail.com>
2026-05-13 22:33:14 -0300

    feat: Creacion del Service

--- demo/src/main/java/com/stretto/demo/features/productionLot/ProductionLotService.java
+++ demo/src/main/java/com/stretto/demo/features/productionLot/ProductionLotService.java
@@ -1,5 +1,16 @@
 package com.stretto.demo.features.productionLot;
 
-public interface ProductionLotService {
+import com.stretto.demo.features.productionLot.domain.dto.ProductionLotDTORequest;
+import com.stretto.demo.features.productionLot.domain.dto.ProductionLotDTOResponse;
 
+import java.util.List;
+
+public interface ProductionLotService {
+    ProductionLotDTOResponse create(ProductionLotDTORequest request);
+
+    List<ProductionLotDTOResponse> findAll();
+
+    ProductionLotDTOResponse findById(Long id);
+
+    ProductionLotDTOResponse update(ProductionLotDTORequest request, Long id);
 }

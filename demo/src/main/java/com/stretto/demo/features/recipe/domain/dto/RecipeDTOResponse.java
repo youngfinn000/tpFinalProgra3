dbc585048b33c5a6c781cface5c596f39375a000
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 10:05:12 -0300

    fix: Se creo el DTO Response

--- demo/src/main/java/com/stretto/demo/features/recipe/domain/dto/RecipeDTOResponse.java
+++ demo/src/main/java/com/stretto/demo/features/recipe/domain/dto/RecipeDTOResponse.java
@@ -1,4 +1,49 @@
 package com.stretto.demo.features.recipe.domain.dto;
 
+import com.stretto.demo.features.productionLot.domain.dto.ProductionLotDTOResponse;
+import com.stretto.demo.features.recipe.domain.enums.UnitMeasurementEnum;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RecipeDTOResponse {
+    private Long id;
+    private String name;
+    private Double baseQuantity;
+    private UnitMeasurementEnum unitBase;
+    private ProductSummary product;
+    private ProductionLotSummary productionLot;
+    private List<IngredientSummary> ingredients;
+    private List<RecipeSummary> subRecipes;
+
+    @Getter
+    @Builder
+    public static class ProductionLotSummary{
+        private Long id;
+    }
+
+    @Getter
+    @Builder
+    public static class ProductSummary{
+        private Long id;
+        private String name;
+    }
+
+    @Getter
+    @Builder
+    public static class IngredientSummary{
+        private Long id;
+    }
+
+    @Getter
+    @Builder
+    public static class RecipeSummary {
+        private Long id;
+        private String name;
+    }
 }

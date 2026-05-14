48d06e9672297f2be0c374b456fde99ee5558761
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 11:16:18 -0300

    fix: Se terminl el Recipe Entity

--- demo/src/main/java/com/stretto/demo/features/recipe/RecipeController.java
+++ demo/src/main/java/com/stretto/demo/features/recipe/RecipeController.java
@@ -1,4 +1,47 @@
 package com.stretto.demo.features.recipe;
 
+import com.stretto.demo.features.recipe.domain.dto.RecipeDTORequest;
+import com.stretto.demo.features.recipe.domain.dto.RecipeDTOResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/recipe")
 public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @PostMapping
+    public ResponseEntity<RecipeDTOResponse> create(@Valid @RequestBody RecipeDTORequest request)
+    {
+        RecipeDTOResponse response = recipeService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecipeDTOResponse>> findAll()
+    {
+        List<RecipeDTOResponse> responses = recipeService.findAll();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RecipeDTOResponse> findById(@PathVariable Long id)
+    {
+        RecipeDTOResponse response = recipeService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RecipeDTOResponse> update(@PathVariable Long id, @RequestBody RecipeDTORequest request)
+    {
+        RecipeDTOResponse response = recipeService.update(request,id);
+        return ResponseEntity.ok(response);
+    }
 }

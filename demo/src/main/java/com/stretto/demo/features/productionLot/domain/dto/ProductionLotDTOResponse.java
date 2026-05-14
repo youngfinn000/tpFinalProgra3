d14d84ed8d5b974e98961acdc10ce8e39ba51273
Emanuel Bustos <fakuj305@gmail.com>
2026-05-13 22:20:53 -0300

    fix: Creacion del DTO Response

--- demo/src/main/java/com/stretto/demo/features/productionLot/domain/dto/ProductionLotDTOResponse.java
+++ demo/src/main/java/com/stretto/demo/features/productionLot/domain/dto/ProductionLotDTOResponse.java
@@ -1,4 +1,32 @@
 package com.stretto.demo.features.productionLot.domain.dto;
 
+import com.stretto.demo.features.productionLot.domain.enums.StatusLotEnum;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductionLotDTOResponse {
+    private Long id;
+    private RecipeSummary recipe;
+    private InternalUserSummary internalUser;
+    private Double amountProduced;
+    private StatusLotEnum status;
+    private Double performancePCT;
+
+    @Getter
+    @Builder
+    public static class RecipeSummary{
+        private Long id;
+        private String name;
+    }
+
+    @Getter
+    @Builder
+    public static class InternalUserSummary{
+        private Long id;
+        private String name;
+    }
 }

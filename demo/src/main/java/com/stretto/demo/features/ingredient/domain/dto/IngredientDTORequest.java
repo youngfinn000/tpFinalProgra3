822110ab3fe73e25cebcad5c4406730750da5418
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 21:45:45 -0300

    fix: DTO Request hecho

--- demo/src/main/java/com/stretto/demo/features/ingredient/domain/dto/IngredientDTORequest.java
+++ demo/src/main/java/com/stretto/demo/features/ingredient/domain/dto/IngredientDTORequest.java
@@ -1,4 +1,24 @@
 package com.stretto.demo.features.ingredient.domain.dto;
 
+import com.stretto.demo.features.recipe.domain.enums.UnitMeasurementEnum;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IngredientDTORequest {
+    @NotNull(message = "Required amount is required")
+    private Double requiredAmount;
+
+    @NotNull(message = "Unit is required")
+    private UnitMeasurementEnum unit;
+
+    @NotNull (message = "Recipe Id is required")
+    private Long recipeId;
+
+    @NotNull(message = "Stock id is required")
+    private Long stockId;
 }

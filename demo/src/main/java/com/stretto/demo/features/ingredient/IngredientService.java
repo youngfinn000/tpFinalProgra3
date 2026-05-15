4bb303bd663f1738f5252fbd8b27c7d9cfdf860f
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 21:56:35 -0300

    fix: Se creo el service

--- demo/src/main/java/com/stretto/demo/features/ingredient/IngredientService.java
+++ demo/src/main/java/com/stretto/demo/features/ingredient/IngredientService.java
@@ -1,4 +1,17 @@
 package com.stretto.demo.features.ingredient;
 
+import com.stretto.demo.features.ingredient.domain.dto.IngredientDTORequest;
+import com.stretto.demo.features.ingredient.domain.dto.IngredientDTOResponse;
+
+import java.util.List;
+
 public interface IngredientService {
+
+    IngredientDTOResponse create(IngredientDTORequest request);
+
+    List<IngredientDTOResponse> findAll();
+
+    IngredientDTOResponse findById(Long id);
+
+    IngredientDTOResponse update(IngredientDTORequest request, Long id);
 }

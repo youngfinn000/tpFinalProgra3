62c77f5f9bea87de89bee696fe9230b7ea8f9a56
Emanuel Bustos <fakuj305@gmail.com>
2026-05-14 22:33:12 -0300

    fix: Se agrego el DTO Request

--- demo/src/main/java/com/stretto/demo/features/stock/domain/dto/StockDTORequest.java
+++ demo/src/main/java/com/stretto/demo/features/stock/domain/dto/StockDTORequest.java
@@ -1,4 +1,26 @@
 package com.stretto.demo.features.stock.domain.dto;
 
+import com.stretto.demo.features.recipe.domain.enums.UnitMeasurementEnum;
+import com.stretto.demo.features.stock.domain.enums.StatusEnum;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class StockDTORequest {
+
+    @NotNull(message = "Current Stock is required")
+    private Double currentStock;
+
+    @NotNull(message = "Minimun stock is required")
+    private Double minimumStock;
+
+    @NotNull(message = "Unit measurement is required")
+    private UnitMeasurementEnum unitMeasurement;
+
+    @NotNull(message = "Status is required")
+    private StatusEnum status;
 }
